cef8c88c0edd7b2647f8a82b1bd38c02f2c7be55
Viktor Kurchenko <vky8411@gmail.com>
2016-08-10 14:46:16 +0300

    Initial commit

---
+++ src/main/java/demo/Application.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * Demo launcher
+ */
+public class Application {
+
+   public static void main(String[] args) {
+
+   }
+
+}
---
+++ src/main/java/global/config/GlobalConfig.java
@@ -0,0 +1,12 @@
+package global.config;
+
+/**
+ * Global configuration interface
+ *
+ * Contains global configuration settings for server and client
+ */
+public interface GlobalConfig {
+
+
+
+}
