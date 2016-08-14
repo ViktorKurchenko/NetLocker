f00c0544f57e0605585553c4b92566ab31442e60
Viktor Kurchenko <vky8411@gmail.com>
2016-08-14 18:40:54 +0300

    Initial commit

---
+++ src/main/java/server/components/I_Server.java
@@ -0,0 +1,7 @@
+package server.components;
+
+/**
+ * Created by Vkurchenko on 8/10/2016.
+ */
+public interface I_Server {
+}
---
+++ src/main/java/server/config/ServerConfig.java
@@ -0,0 +1,7 @@
+package server.config;
+
+/**
+ * Created by Vkurchenko on 8/14/2016.
+ */
+public interface ServerConfig {
+}
